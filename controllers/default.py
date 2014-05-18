aac4aa865add1373a867df191d04a69cf95859dd
katjwinter <katjwinter@gmail.com>
2014-05-18 11:22:46 -0400

    Update default.py

--- controllers/default.py
+++ controllers/default.py
@@ -2,11 +2,7 @@
 # this file is released under public domain and you can use without limitations
 
 #########################################################################
-## This is a samples controller
-## - index is the default action of any application
-## - user is required for authentication and authorization
-## - download is for downloading files uploaded in the db (does streaming)
-## - call exposes all registered services (none by default)
+## Main controller file for app operations.
 #########################################################################
 import urllib2
 import json
